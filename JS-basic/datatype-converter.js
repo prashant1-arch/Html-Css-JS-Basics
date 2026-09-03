279f60cf14a6a69cac936161872a4779c435b222
prashant1-arch <ps4878383@gmail.com>
2026-09-03 20:35:15 +0530

    Thursday push

---
+++ JS-basic/datatype-converter.js
@@ -0,0 +1,5 @@
+let str = "100";
+let num = Number(str);
+
+console.log(num);        // 100
+console.log(typeof num); // "number"
---
+++ JS-basic/truthy-values.js
@@ -0,0 +1,11 @@
+if ("Hello") {
+    console.log("Truthy");
+}
+
+if (10) {
+    console.log("Truthy");
+}
+
+if ([]) {
+    console.log("Truthy");
+}
