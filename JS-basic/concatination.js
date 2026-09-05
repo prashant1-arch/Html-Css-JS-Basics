8a0fb92aaccf25c2921dd2e10eb8b6f1955e9a4b
prashant1-arch <ps4878383@gmail.com>
2026-09-05 11:53:25 +0530

    Friday push

---
+++ JS-basic/concatination.js
@@ -0,0 +1,18 @@
+console.log(10 + "20");
+console.log(9 - "5");
+console.log("Java" + "Script");
+console.log(" " + " ");
+let sum = " " + 0;
+console.log(sum);
+console.log(typeof sum);
+console.log("vinod" - "thapa");
+console.log(true + true);
+console.log(true + false);
+console.log(false + true);
+console.log(false - true);
+
+// let sum = "5" + 10;
+// console.log(sum);
+
+// const str = "Hello " + "World";
+// console.log(str);    
