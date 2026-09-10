f03b33f52b079eb95ec278f03106188dae26186b
prashant1-arch <ps4878383@gmail.com>
2026-09-10 20:57:32 +0530

    new push

---
+++ JS-basic/Function/Add.js
@@ -0,0 +1,7 @@
+function sum(a, b) {
+  return a + b;
+}
+
+console.log(sum(5, 5));
+console.log(sum(15, 50));
+console.log(sum(25, 750));
---
+++ JS-basic/Function/Party.js
@@ -0,0 +1,7 @@
+//! Write a JavaScript program that defines a function called greet to welcome individuals to the Prashant's party. The function should take a name parameter and output the message "Hello [name], Welcome to Prashant's party". Call the function twice, once with the argument "vinod" and once with the argument "ram".
+function greet(name) {
+  console.log("Hello " + name + ", Welcome to Prashant's party");
+}
+
+greet("ram");
+greet("sita");
